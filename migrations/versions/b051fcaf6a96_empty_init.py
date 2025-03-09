ef1975cf5b7b42d7113b6f409b37e6cc1ac4a210
fahmy-dev <faahmy@proton.me>
2025-03-09 21:32:17 +0300

    commit

---
+++ migrations/versions/b051fcaf6a96_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: b051fcaf6a96
+Revises: 
+Create Date: 2025-03-09 20:19:21.199320
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b051fcaf6a96'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
